171744610a8ac7984b8dc94b3e73b9613a743267
sthefanyvillafranca <svillsei@myuax.com>
2026-03-02 12:07:33 +0100

    feat (main): creación del main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main {
+    public static void main(String[] args) {
+    }
 }
+
+/**
+ *
+ * Biblioteca --> gestionar libros
+ * Libro --> id, titulo, autor, num pag.
+ *
+ * CRUD - create , read, update delete
+ * addLibro
+ * deleteLibro
+ * updateNumPags
+ * findLibro
+ * deleteLibro
+ *
+ * obtainCatalogo (todos los libros que tenemos)
+ *
+ * ¿Cómo se guardaran los libros? Un array.
+ * Libro [] catalogo = new Libro [longitud]
+ *
+ */
+
+
+/**
+ * Recordar que al publicar el repositorio en github por primera vez, poner chore: estructura o algo asi
+ */
