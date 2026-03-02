1fc8160dcae22ac348a45390683518a84ecad6e2
sthefanyvillafranca <svillsei@myuax.com>
2026-03-02 12:00:09 +0100

    feat (Libro): Creada la clase Libro con constructor, getters, toString

--- src/Libro.java
+++ src/Libro.java
@@ -1,4 +1,75 @@
-package PACKAGE_NAME;
-
 public class Libro {
+   /* Libro --> id, titulo, autor, num pag. */
+
+    /*ATRIBUTOS de la CLASE*/
+    private int id;
+    private String titulo;
+    private String autor;
+    private int numPaginas;
+
+    /**
+     * Metodo constructor
+     * @param id identificador del libro
+     * @param titulo titulo del libro
+     * @param autor nombre del autor
+     * @param numPaginas número de paginas
+     */
+    public Libro(int id, String titulo, String autor, int numPaginas) {
+        /* PARAMETROS del metodo */
+
+        int numHojas; // variable del metodo
+
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+
+        numHojas = numPaginas/2;
+
+    }
+
+    /**
+     * Devuelve el id del libro
+     * @return id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Devuelve el titulo del libro
+     * @return titulo
+     */
+    public String getTitulo() {
+        return titulo;
+    }
+
+    /**
+     * Devuelve el autor
+     * @return autor
+     */
+    public String getAutor() {
+        return autor;
+    }
+
+    /**
+     * Devuelve número de paginas
+     * @return numPaginas
+     */
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    // recordar siempre debe estar el Override.
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Libro: {");
+        sb.append(String.format("\n id: %d;", id));
+        sb.append(String.format(" titulo: %s;", titulo));
+        sb.append(String.format(" autor: %s;", autor));
+        sb.append(String.format(" num. páginas: %d;", numPaginas));
+        sb.append("}");
+
+        return sb.toString();
+    }
 }
