9ce48b6cdb709a4d11b158fa5e5d5dc4cac62126
sthefanyvillafranca <svillsei@myuax.com>
2026-03-02 12:44:13 +0100

    feat (Biblioteca) Pruebas inciales main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca(10);
+        Libro libro1 = new Libro(1,"El quijote", "Miguelito", 900);
+        Libro libro2 = new Libro(2,"El principito", "Frances", 100);
+
+        System.out.print(biblioteca.findLibro(1)); //
+        System.out.print(biblioteca.addLibro(libro1)); //
+        System.out.print(biblioteca.addLibro(libro2)); //
+
+        System.out.print(biblioteca.obtainCatalogo());
+
+
     }
 }
 
